8da567ed9b74bb275e9f1bfd28d5ad10612df57e
cmmedoro <s271882@studenti.polito.it>
2024-03-28 13:18:48 +0100

    Added code to visualize reconstruction

--- preprocessing.py
+++ preprocessing.py
@@ -150,8 +150,8 @@ def create_train_eval_sequences(dataframe, time_steps):
         # find the end of this sequence
         end_ix = i + time_steps
         # check if we are beyond the dataset length for this building
-        if end_ix > len(building_data)-1:
-          break
+        #if end_ix > len(building_data)-1:
+         # break
         output.append(building_data[i : (i + time_steps),:])
         output2.append(building_data[i : (i + time_steps),0])
   return np.stack(output), np.stack(output2)
@@ -171,5 +171,42 @@ def create_test_sequences(dataframe, time_steps):
         output.append(building_data[i : end_ix,:])
         output2.append(building_data[i : end_ix,0])
     return np.stack(output), np.stack(output2)
+
+
+# Define a function to revert the sliding window application
+def reconstruction_windows(timeseries):
+  """
+  This function takes as input the windows regarding a single time series in the original dataset.
+  Input: timeseries.shape = (8713, 72), where 72 = window_size, 8713 = number_of_windows = 24*366 - window_size + 1
+  Output: media.shape = (24*366, 1) ---> meter_reading column reconstructed, by averaging the reconstructions for the same points
+  """
+  df_x_train = pd.DataFrame(np.squeeze(timeseries))
+  transposed_df = df_x_train.T
+  nan_rows = np.full((8784-transposed_df.shape[0],transposed_df.shape[1]),np.nan)
+  rows = pd.DataFrame(nan_rows)
+  transposed_df = pd.concat([transposed_df, rows], ignore_index = True) #transposed_df.append(rows, ignore_index = True)
+  for i in range(0, transposed_df.shape[1]):
+    col = transposed_df.iloc[:, i].shift(i)
+    transposed_df.iloc[:, i] = col
+  retransposed = transposed_df.T
+  media = np.nanmean(retransposed, axis = 0)
+  return media
+
+def apply_reconstruction(dataframe, n_timeseries):
+  # Take the entire dataframe
+  squeezed_df = np.squeeze(dataframe)
+  # Reshape it: ex. train (162, 8713, 72) ---> 162 = number of timeseries in the dataframe
+  reshaped = np.reshape(squeezed_df, (n_timeseries, 8713, 72))
+  reconstruction = []
+  i = 0
+  for timeseries in reshaped:
+    # Reconstruct separately each timeseries
+    print(i)
+    average_reconstruction = reconstruction_windows(timeseries)
+    reconstruction.append(average_reconstruction)
+    i = i+1
+  final_reconstruction = np.squeeze(reconstruction)
+  fr = np.reshape(final_reconstruction, (n_timeseries*8784, 1))
+  return fr
   
 
