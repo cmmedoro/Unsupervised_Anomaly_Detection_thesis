62a5d9dbac09fa44209a9d9162edffe3336c7692
cmmedoro <s271882@studenti.polito.it>
2024-04-03 19:59:49 +0200

    Primarly updated notebooks. Updated preprocessing.

--- preprocessing.py
+++ preprocessing.py
@@ -158,6 +158,23 @@ def create_train_eval_sequences(dataframe, time_steps):
         output2.append(building_data[i : (i + time_steps),0])
   return np.stack(output), np.stack(output2)
 
+def create_multivariate_train_eval_sequences(dataframe, time_steps):
+  scaler = MinMaxScaler(feature_range=(0,1))
+  output = []
+  output2=[]
+  for building_id, gdf in dataframe.groupby("building_id"):
+      gdf[['meter_reading', 'sea_level_pressure', 'weekday_x', 'weekday_y']] = scaler.fit_transform(gdf[['meter_reading', 'sea_level_pressure', 'weekday_x', 'weekday_y']])
+      building_data = np.array(gdf[['meter_reading', 'sea_level_pressure', 'weekday_x', 'weekday_y', 'is_holiday']]).astype(float) 
+      for i in range(len(building_data) - time_steps + 1):
+        # find the end of this sequence
+        end_ix = i + time_steps
+        # check if we are beyond the dataset length for this building
+        if end_ix > len(building_data)-1:
+          break
+        output.append(building_data[i : (i + time_steps),:])
+        output2.append(building_data[i : (i + time_steps),0])
+  return np.stack(output), np.stack(output2)
+
 # Generated testing sequences for use in the model.
 def create_test_sequences(dataframe, time_steps):
     scaler = MinMaxScaler(feature_range=(0,1))
@@ -174,54 +191,19 @@ def create_test_sequences(dataframe, time_steps):
         output2.append(building_data[i : end_ix,0])
     return np.stack(output), np.stack(output2)
 
+def create_multivariate_test_sequences(dataframe, time_steps):
+    scaler = MinMaxScaler(feature_range=(0,1))
+    output = []
+    output2 = []
+    for building_id, gdf in dataframe.groupby("building_id"):
+       gdf[['meter_reading', 'sea_level_pressure', 'weekday_x', 'weekday_y']] = scaler.fit_transform(gdf[['meter_reading', 'sea_level_pressure', 'weekday_x', 'weekday_y']])
+       building_data = np.array(gdf[['meter_reading', 'sea_level_pressure', 'weekday_x', 'weekday_y', 'is_holiday']]).astype(float) 
+       for i in range(0, len(building_data) - time_steps + 1, time_steps):
+        end_ix = i + time_steps
+        #if end_ix > len(gdf):
+          #break
+        output.append(building_data[i : end_ix,:])
+        output2.append(building_data[i : end_ix,0])
+    return np.stack(output), np.stack(output2)
 
-# Define a function to revert the sliding window application
-def reconstruction_windows(timeseries):
-  """
-  This function takes as input the windows regarding a single time series in the original dataset.
-  Input: timeseries.shape = (8713, 72), where 72 = window_size, 8713 = number_of_windows = 24*366 - window_size + 1
-  Output: media.shape = (24*366, 1) ---> meter_reading column reconstructed, by averaging the reconstructions for the same points
-  """
-  df_x_train = pd.DataFrame(np.squeeze(timeseries))
-  transposed_df = df_x_train.T
-  nan_rows = np.full((8784-transposed_df.shape[0],transposed_df.shape[1]),np.nan)
-  rows = pd.DataFrame(nan_rows)
-  transposed_df = pd.concat([transposed_df, rows], ignore_index = True) #transposed_df.append(rows, ignore_index = True)
-  for i in range(0, transposed_df.shape[1]):
-    col = transposed_df.iloc[:, i].shift(i)
-    transposed_df.iloc[:, i] = col
-  retransposed = transposed_df.T
-  media = np.nanmean(retransposed, axis = 0)
-  return media
 
-def apply_reconstruction(dataframe, n_timeseries):
-  # Take the entire dataframe
-  squeezed_df = np.squeeze(dataframe)
-  # Reshape it: ex. train (162, 8713, 72) ---> 162 = number of timeseries in the dataframe
-  reshaped = np.reshape(squeezed_df, (n_timeseries, 8712, 72)) #8713
-  reconstruction = []
-  i = 0
-  for timeseries in reshaped:
-    # Reconstruct separately each timeseries
-    print(i)
-    average_reconstruction = reconstruction_windows(timeseries)
-    reconstruction.append(average_reconstruction)
-    i = i+1
-  final_reconstruction = np.squeeze(reconstruction)
-  fr = np.reshape(final_reconstruction, (n_timeseries*8784, 1))
-  return fr
-  
-def padding_w(w, batch_size):
-  # This function needs to be used on the outputs of the decoders: the last "batch" is not going to be full of the batch_size elements 
-  # characterizing a single batch, therefore a proper padding needs to be ensured. Moreover, with this function, we also transform
-  # the padded output into a suitable format to perform the following operations to obtain the reconstructed input
-  last = w[-1]
-  padded_last = F.pad(last, (0, 0, 0, batch_size-w.size()[0]))
-  new_lista = w[:-1]
-  new_lista.append(padded_last)
-  res_w = torch.cat(new_lista, dim=0)
-  index = batch_size-w.size()[0]
-  original_rec = res_w[:-index]
-  origin_rec = original_rec.detach().cpu().numpy()
-
-  return origin_rec
