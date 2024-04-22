ac2d46341c0f6d55f0bb3e748c9c082f476501be
cmmedoro <s271882@studenti.polito.it>
2024-04-22 12:23:47 +0200

    Updated train file

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.utils.data as data_utils
-from USAD.usad import *
+#from USAD.usad import *
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, roc_auc_score
@@ -12,6 +12,8 @@ from postprocessing import *
 import plotly.graph_objects as go
 import torch.utils.data as data_utils
 #from USAD.usad_conv import *
+from lstm_ae import *
+from utils_ae import *
 import parser_file
 
 device = get_default_device()
@@ -49,20 +51,25 @@ BATCH_SIZE =  args.batch_size
 N_EPOCHS = args.epochs
 hidden_size = args.hidden_size
 
+# batch_size, window_length, n_features = X_train.shape
+batch, window_len, n_channels = X_train.shape
+
 w_size = X_train.shape[1] * X_train.shape[2]
 z_size = int(w_size * hidden_size) 
 
 model_type = args.model_type
 
-if model_type == "conv_ae":
+if model_type == "conv_ae" or model_type == "lstm_ae":
     train_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_train).float().view(([X_train.shape[0], w_size, 1]))), batch_size = BATCH_SIZE, shuffle = False, num_workers = 0)
     val_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_val).float().view(([X_val.shape[0],w_size, 1]))) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 else:
     train_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_train).float().view(([X_train.shape[0], w_size]))), batch_size = BATCH_SIZE, shuffle = False, num_workers = 0)
     val_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_val).float().view(([X_val.shape[0],w_size]))) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
+if model_type == "lstm_ae":
+    z_size = 32
 # Create the model and send it on the gpu device
-model = UsadModel(w_size, z_size)
+model = LstmAE(n_channels, z_size, train_window)
 model = to_device(model, device)
 print(model)
 
@@ -73,6 +80,6 @@ np.save('/nfs/home/medoro/Unsupervised_Anomaly_Detection_thesis/checkpoints/hist
 checkpoint_path = args.save_checkpoint_dir
 torch.save({
         'encoder': model.encoder.state_dict(),
-        'decoder1': model.decoder1.state_dict(),
-        'decoder2': model.decoder2.state_dict()
+        'decoder': model.decoder.state_dict(),
+        #'decoder2': model.decoder2.state_dict()
         }, checkpoint_path) # the path should be set in the run.job file
