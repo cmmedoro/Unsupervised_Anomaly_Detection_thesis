f99d4aa7867b5ee950c34427acf7c0aaf378b2cc
cmmedoro <s271882@studenti.polito.it>
2024-04-23 10:58:07 +0200

    Updates

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ device = get_default_device()
 #### Open the dataset ####
 # Original dataset
 #energy_df = pd.read_csv("/nfs/home/medoro/Unsupervised_Anomaly_Detection_thesis/data/train.csv")
-energy_df = pd.read_csv("/content/drive/MyDrive/Backup_Carla_tesi_sorry_bisogno_di_gpu/train_features.csv")
+energy_df = pd.read_csv("/content/drive/MyDrive/ADSP/Backup_tesi_Carla_sorry_bisogno_di_gpu/train_features.csv")
 # Select some columns from the original dataset
 df = energy_df[['building_id','primary_use', 'timestamp', 'meter_reading', 'sea_level_pressure', 'is_holiday','anomaly']]
 
@@ -62,7 +62,7 @@ test = pd.concat(dfs_test.values())
 if args.do_resid:
     # Residuals dataset (missing values and dates imputation already performed)
     #residuals = pd.read_csv("/nfs/home/medoro/Unsupervised_Anomaly_Detection_thesis/data/residuals2.csv")
-    residuals = pd.read_csv("/content/drive/MyDrive/Backup_Carla_tesi_sorry_bisogno_di_gpu/residuals.csv")
+    residuals = pd.read_csv("/content/drive/MyDrive/ADSP/Backup_tesi_Carla_sorry_bisogno_di_gpuesiduals.csv")
     residui_df = residuals[['timestamp', 'building_id', 'primary_use', 'anomaly', 'meter_reading', 'sea_level_pressure', 'is_holiday', 'resid']]
     dfs_train, dfs_val, dfs_test = train_val_test_split(residui_df)
     train = pd.concat(dfs_train.values())
