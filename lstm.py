fb585a542b2b0c2c07b885f24db2c23a44b9c888
cmmedoro <s271882@studenti.polito.it>
2024-05-08 17:20:23 +0200

    Added code for forecasting. To be updated

---
+++ lstm.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+
+from utils_ae import *
+device = get_default_device()
+
+class LstmModel(nn.Module):
+  def __init__(self, in_size, latent_size): 
+    super().__init__()
+    """
+    in_size: number of features in input
+    latent_size: size of the latent space of the lstm
+    Ex. in_size = 5, latent_size = 50
+    """
+    self.lstm = nn.LSTM(input_size=in_size, hidden_size=latent_size, num_layers=1, batch_first=True, dropout = 0.2
+            # input and output tensors are provided as (batch, seq_len, feature(size))
+        )
+    self.fc = nn.Linear(latent_size, 1)
+  def forward(self, w):
+    #print("Input E: ", w.size())
+    z, (h_n, c_n) = self.lstm(w)
+    #print("Output E: ", h_n.size())
+    output = self.fc(h_n)
+    return output
+  
+  def training_step(self, batch, criterion, n):
+    z = self(batch)
+    loss = criterion(z, batch)#torch.mean((batch-w)**2) #loss = mse
+    return loss
+
+  def validation_step(self, batch, criterion, n):
+    with torch.no_grad():
+        z = self(batch)
+        loss = criterion(z, batch)#torch.mean((batch-w)**2) #loss = mse
+    return loss
+        
+  """def validation_epoch_end(self, outputs):
+    batch_losses = [x for x in outputs]
+    epoch_loss = torch.stack(batch_losses).mean()
+    return {'val_loss': epoch_loss.item()}"""
+    
+  def epoch_end(self, epoch, result, result_train):
+    print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}".format(epoch, result_train, result))
+    
+def evaluate(model, val_loader, criterion, n):
+    batch_loss = []
+    for [batch] in val_loader:
+       batch = to_device(batch, device)
+       loss = model.validation_step(batch, criterion, n) 
+       batch_loss.append(loss)
+
+    epoch_loss = torch.stack(batch_loss).mean()
+    return epoch_loss
+
+
+def training(epochs, model, train_loader, val_loader, opt_func=torch.optim.Adam): 
+    history = []
+    optimizer = opt_func(model.parameters())
+    criterion = nn.MSELoss().to(device) #nn.KLDivLoss(reduction="batchmean").to(device) #nn.MSELoss().to(device)
+    for epoch in range(epochs):
+        for [batch] in train_loader:
+            batch=to_device(batch,device)
+            optimizer.zero_grad()
+
+            loss = model.training_step(batch, criterion, epoch+1)
+            loss.backward()
+            optimizer.step()
+            #optimizer.zero_grad()
+            
+            
+        result= evaluate(model, val_loader, criterion, epoch+1)
+        result_train = evaluate(model, train_loader, criterion, epoch+1)
+        model.epoch_end(epoch, result, result_train)
+        #model.epoch_end(epoch, result)
+        history.append(result)
+    return history 
+    
+def testing(model, test_loader):
+    results=[]
+    reconstruction = []
+    criterion = nn.MSELoss().to(device) #nn.KLDivLoss(reduction="batchmean").to(device)
+    with torch.no_grad():
+        for [batch] in test_loader: 
+            batch=to_device(batch,device)
+            w=model(batch)
+            results.append(criterion(w, batch))
+            #results.append(torch.mean((batch-w)**2,axis=1))
+            reconstruction.append(w)
+    return results, reconstruction
---
+++ train_forecasting.py
@@ -0,0 +1,167 @@
+from preprocessing import *
+import preprocessing as prp
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.utils.data as data_utils
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, roc_auc_score
+from postprocessing import *
+import plotly.graph_objects as go
+import torch.utils.data as data_utils
+import parser_file
+import warnings
+warnings.filterwarnings('ignore')
+from utils_ae import *
+from lstm import *
+
+args = parser_file.parse_arguments()
+
+model_type = args.model_type    
+
+device = get_default_device()
+
+#### Open the dataset ####
+# Original dataset
+#energy_df = pd.read_csv("/nfs/home/medoro/Unsupervised_Anomaly_Detection_thesis/data/train.csv")
+energy_df = pd.read_csv("/content/drive/MyDrive/ADSP/Backup_tesi_Carla_sorry_bisogno_di_gpu/train_features.csv")
+# Select some columns from the original dataset
+df = energy_df[['building_id','primary_use', 'timestamp', 'meter_reading', 'sea_level_pressure', 'is_holiday','anomaly', 'air_temperature']]
+
+### PREPROCESSING ###
+# 1) Impute missing values
+imputed_df = impute_nulls(df)
+# 2) Add trigonometric features
+df = add_trigonometric_features(imputed_df)
+
+# 3) Resample the dataset: measurement frequency = "1h"
+dfs_dict = impute_missing_dates(df)
+df1 = pd.concat(dfs_dict.values())
+#lags = [1, -1]
+#df1 = create_diff_lag_features(df1, lags)
+
+# Split the dataset into train, validation and test
+dfs_train, dfs_val, dfs_test = train_val_test_split(df1)
+train = pd.concat(dfs_train.values())
+val = pd.concat(dfs_val.values())
+test = pd.concat(dfs_test.values())
+
+if args.do_resid:
+    # Residuals dataset (missing values and dates imputation already performed)
+    #residuals = pd.read_csv("/nfs/home/medoro/Unsupervised_Anomaly_Detection_thesis/data/residuals2.csv")
+    # ADSP/Backup_tesi_Carla_sorry_bisogno_di_gpu
+    residuals = pd.read_csv("/content/drive/MyDrive/ADSP/Backup_tesi_Carla_sorry_bisogno_di_gpu/residuals.csv")
+    residui_df = residuals[['timestamp', 'building_id', 'primary_use', 'anomaly', 'meter_reading', 'sea_level_pressure', 'is_holiday', 'resid']]
+    dfs_train, dfs_val, dfs_test = train_val_test_split(residui_df)
+    train = pd.concat(dfs_train.values())
+    val = pd.concat(dfs_val.values())
+    test = pd.concat(dfs_test.values())
+print(train.columns)
+if args.do_multivariate:
+    residuals = pd.read_csv("/content/drive/MyDrive/ADSP/Backup_tesi_Carla_sorry_bisogno_di_gpu/residuals.csv")
+    residui_df = residuals[['timestamp', 'building_id', 'primary_use', 'anomaly', 'meter_reading', 'sea_level_pressure', 'is_holiday', 'resid']]
+    dfs_train, dfs_val, dfs_test = train_val_test_split(residui_df)
+    train = pd.concat(dfs_train.values())
+    val = pd.concat(dfs_val.values())
+    test = pd.concat(dfs_test.values())
+
+def create_train_eval_sequences(dataframe, time_steps):
+  scaler = MinMaxScaler(feature_range=(0,1))
+  output = []
+  output2=[]
+  for building_id, gdf in dataframe.groupby("building_id"):
+      gdf[['meter_reading']] = scaler.fit_transform(gdf[['meter_reading']])
+      building_data = np.array(gdf[['meter_reading']]).astype(float) 
+      for i in range(len(building_data)):
+        # find the end of this sequence
+        end_ix = i + time_steps
+        # check if we are beyond the dataset length for this building
+        if end_ix > len(building_data)-1:
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = building_data[i:end_ix, :], building_data[end_ix, 0]
+        output.append(seq_x)
+        output2.append(seq_y)
+  return np.stack(output), np.stack(output2)
+
+def create_multivariate_train_eval_sequences(dataframe, time_steps):
+  scaler = MinMaxScaler(feature_range=(0,1))
+  output = []
+  output2=[]
+  for building_id, gdf in dataframe.groupby("building_id"):
+      gdf[['meter_reading', 'sea_level_pressure', 'air_temperature', 'weekday_x', 'weekday_y']] = scaler.fit_transform(gdf[['meter_reading', 'sea_level_pressure', 'air_temperature','weekday_x', 'weekday_y']])
+      building_data = np.array(gdf[['meter_reading', 'sea_level_pressure', 'air_temperature', 'weekday_x', 'weekday_y']]).astype(float) 
+      for i in range(len(building_data)):
+        # find the end of this sequence
+        end_ix = i + time_steps
+        # check if we are beyond the dataset length for this building
+        if end_ix > len(building_data)-1:
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = building_data[i:end_ix, :], building_data[end_ix, 0]
+        output.append(seq_x)
+        output2.append(seq_y)
+  return np.stack(output), np.stack(output2)
+
+### TRAINING THE MODEL ###
+# For training we are going to create an input dataset consisting of overlapping windows of 72 measurements (3 days)
+train_window = args.train_window
+if args.do_multivariate:
+    X_train, y_train = create_multivariate_train_eval_sequences(train, train_window)
+    X_val, y_val = create_multivariate_train_eval_sequences(val, train_window)
+else:
+    X_train, y_train = create_train_eval_sequences(train, train_window)
+    X_val, y_val = create_train_eval_sequences(val, train_window)
+
+
+BATCH_SIZE =  args.batch_size
+N_EPOCHS = args.epochs
+hidden_size = args.hidden_size
+
+# batch_size, window_length, n_features = X_train.shape
+batch, window_len, n_channels = X_train.shape
+
+w_size = X_train.shape[1] * X_train.shape[2]
+z_size = int(w_size * hidden_size) 
+
+if model_type == "conv_ae" or model_type == "lstm_ae" or model_type == "usad_conv" or model_type == "usad_lstm" or model_type == "vae":
+    #Credo di dover cambiare X_train.shape[0], w_size, X_train.shape[2] con X_train.shape[0], X_train.shape[1], X_train.shape[2]
+    train_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_train).float().view(([X_train.shape[0], X_train.shape[1], X_train.shape[2]]))), batch_size = BATCH_SIZE, shuffle = False, num_workers = 0)
+    val_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_val).float().view(([X_val.shape[0],X_train.shape[1], X_train.shape[2]]))) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
+else:
+    train_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_train).float().view(([X_train.shape[0], w_size]))), batch_size = BATCH_SIZE, shuffle = False, num_workers = 0)
+    val_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(torch.from_numpy(X_val).float().view(([X_val.shape[0],w_size]))) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
+
+
+
+print(device)
+#model = to_device(model, device)
+#print(model)
+
+# Start training
+#history, train_recos = training(N_EPOCHS, model, train_loader, val_loader)
+#print(history)
+#if model_type == "lstm_ae" or model_type == "conv_ae" or model_type == "vae":
+   # history_to_save = torch.stack(history).flatten().detach().cpu().numpy()
+    #train_recos_to_save = np.concatenate([torch.stack(train_recos[:-1]).flatten().detach().cpu().numpy(), train_recos[-1].flatten().detach().cpu().numpy()])
+    #train_recos_to_save = torch.stack(train_recos).flatten().detach().cpu().numpy()
+    # /nfs/home/medoro/Unsupervised_Anomaly_Detection_thesis/checkpoints/history_lstm.npy
+    #np.save('/content/checkpoints/history_vae.npy', history_to_save) #/content/checkpoints er prove su drive
+    #np.save('/content/checkpoints/train_recos.npy', train_recos_to_save)
+#else:
+    ##np.save('/content/checkpoints/history_vae.npy', history)
+model = print("Hello")
+#plot_history(history)
+checkpoint_path = args.save_checkpoint_dir
+if model_type.startswith("usad"):
+    torch.save({
+        'encoder': model.encoder.state_dict(),
+        'decoder1': model.decoder1.state_dict(),
+        'decoder2': model.decoder2.state_dict()
+        }, checkpoint_path) # the path should be set in the run.job file
+else:
+    torch.save({
+            'encoder': model.encoder.state_dict(),
+            'decoder': model.decoder.state_dict()
+            }, checkpoint_path) # the path should be set in the run.job file
