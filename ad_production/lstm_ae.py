ec078df091cfad1ec07d70be5626668507df3473
cmmedoro <s271882@studenti.polito.it>
2024-10-23 11:14:51 +0200

    Updates on production ad

--- ad_production/lstm_ae.py
+++ ad_production/lstm_ae.py
@@ -58,14 +58,14 @@ class LstmAE(nn.Module):
   def training_step(self, batch, criterion, n):
     z = self.encoder(batch)
     w = self.decoder(z)
-    loss = criterion(w, batch)#torch.mean((batch-w)**2) #loss = mse
+    loss = criterion(w, batch)
     return loss
 
   def validation_step(self, batch, criterion, n):
     with torch.no_grad():
         z = self.encoder(batch)
         w = self.decoder(z)
-        loss = criterion(w, batch)#torch.mean((batch-w)**2) #loss = mse
+        loss = criterion(w, batch)
     return loss
     
   def epoch_end(self, epoch, result, result_train):
@@ -74,7 +74,7 @@ class LstmAE(nn.Module):
 def evaluate(model, val_loader, criterion, device, n):
     batch_loss = []
     for [batch] in val_loader:
-       batch = batch.to(device) # to_device(batch, device)
+       batch = batch.to(device) 
        loss = model.validation_step(batch, criterion, n) 
        batch_loss.append(loss)
 
@@ -85,25 +85,21 @@ def evaluate(model, val_loader, criterion, device, n):
 def training(epochs, model, train_loader, val_loader, device, opt_func=torch.optim.Adam): 
     history = []
     optimizer = opt_func(list(model.encoder.parameters())+list(model.decoder.parameters()))
-    #criterion = nn.MSELoss().to(device) #nn.KLDivLoss(reduction="batchmean").to(device) #nn.MSELoss().to(device)
     criterion = nn.L1Loss().to(device)
     for epoch in range(epochs):
         train_loss = []
         for [batch] in train_loader:
-            batch = batch.to(device) #to_device(batch,device)
+            batch = batch.to(device) 
             optimizer.zero_grad()
 
             loss = model.training_step(batch, criterion, epoch+1)
             train_loss.append(loss)
             loss.backward()
             optimizer.step()
-            #optimizer.zero_grad()
         result_train = torch.stack(train_loss).mean()    
             
         result= evaluate(model, val_loader, criterion, device, epoch+1)
-        #result_train = evaluate(model, train_loader, criterion, device, epoch+1)
         model.epoch_end(epoch, result, result_train)
-        #model.epoch_end(epoch, result)
         res = result_train.item()
         history.append((res, result.item()))
     return history 
@@ -111,10 +107,10 @@ def training(epochs, model, train_loader, val_loader, device, opt_func=torch.opt
 def testing(model, test_loader, device):
     results=[]
     reconstruction = []
-    criterion = nn.MSELoss().to(device) #nn.KLDivLoss(reduction="batchmean").to(device)
+    criterion = nn.MSELoss().to(device)
     with torch.no_grad():
         for [batch] in test_loader: 
-            batch = batch.to(device) #to_device(batch,device)
+            batch = batch.to(device) 
             w=model.decoder(model.encoder(batch))
             # Need to squeeze the batch and reconstruction to compute correctly the anomaly score
             # This because the input and the reconstruction are 3-D tensors, so we need to turn them into 2-D
